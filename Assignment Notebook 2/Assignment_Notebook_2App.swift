a4f78cc453c59e0b71943b4242a272c63fab4916
Zuhair Ali <zuhairali20003@gmail.com>
2021-04-26 23:45:26 -0500

    Initial Commit

---
+++ Assignment Notebook 2/Assignment_Notebook_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_Notebook_2App.swift
+//  Assignment Notebook 2
+//
+//  Created by Student on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_Notebook_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment Notebook 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment Notebook 2
+//
+//  Created by Student on 4/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
